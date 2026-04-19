6631ed814ff173521af6de41b2fbe119f6a84bd5
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2026-04-19 14:53:39 -0400

    Bump app version to 1.0.7

--- scraper/app_update.py
+++ scraper/app_update.py
@@ -14,7 +14,7 @@ from urllib.parse import urlparse
 import requests
 
 APP_NAME = "TrustBridgeLeadBuilder"
-APP_VERSION = "1.0.5"
+APP_VERSION = "1.0.7"
 UPDATE_CONFIG_FILENAME = "update_config.json"
 DEFAULT_MANIFEST_URL = "https://trustbridge-manifest.vercel.app/latest.json"
 
